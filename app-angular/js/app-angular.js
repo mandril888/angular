949a15678f00aa9bf85a7126b0a4f30ec305298f
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-07-22 17:30:43 +0200

    app-angular upload

---
+++ app-angular/js/app-angular.js
@@ -0,0 +1,1 @@
+angular.module( 'app-angular', [ 'ngRoute', 'allCtrls', 'configRouting'])
---
+++ app-angular/js/ctrls-app.js
@@ -0,0 +1,21 @@
+angular.module( 'allCtrls', [] )
+
+	.controller( 'homeCtrl' , function ( $scope ) {
+
+
+	})
+
+	.controller( 'aboutCtrl' , function ( $scope ) {
+
+		
+	})
+
+	.controller( 'formCtrl' , function ( $scope ) {
+
+		
+	})
+
+	.controller( 'clientCtrl' , function ( $scope ) {
+
+		
+	})
