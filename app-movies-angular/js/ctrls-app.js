cd51723c5930c7d1e2b957af6214e70b1f1b000e
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-07-23 15:46:31 +0200

    upload2 app-movies-angular

--- app-movies-angular/js/ctrls-app.js
+++ app-movies-angular/js/ctrls-app.js
@@ -3,7 +3,7 @@ angular.module( 'allCtrls', [] )
 	.controller( 'movieCollectionCtrl' , function ( $scope, $rootScope, $http ) {
 
 		// global var to view in index navbar the actual page
-		$rootScope.section = "MovieCollection";
+		$rootScope.section = "Movie Collection";
 
 		$scope.movieCollection = "";
 
@@ -17,7 +17,7 @@ angular.module( 'allCtrls', [] )
 	.controller( 'popularMoviesCtrl' , function ( $scope, $rootScope, $http ) {
 
 		// global var to view in index navbar the actual page
-		$rootScope.section = "PopularMovies";
+		$rootScope.section = "Popular Movies";
 
 		$scope.popularMovies = "";
 
@@ -33,11 +33,11 @@ angular.module( 'allCtrls', [] )
 	.controller( 'upcomingMoviesCtrl' , function ( $scope, $rootScope, $http ) {
 
 		// global var to view in index navbar the actual page
-		$rootScope.section = "UpcomingMovies";
+		$rootScope.section = "Upcoming Movies";
 
 		$scope.upcomingMovies = "";
 
-		var urlUpcomingMovies = 'http://api.themoviedb.org/3/discover/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&sort_by=upcoming.desc';
+		var urlUpcomingMovies = 'http://api.themoviedb.org/3/discover/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&sort_by=release_date.desc';
 		$http.get( urlUpcomingMovies )
 			.then( function( dataUpcomingMovies ) {
 				$scope.upcomingMovies = dataUpcomingMovies.data.results;
@@ -48,13 +48,31 @@ angular.module( 'allCtrls', [] )
 
 	.controller( 'nowPlayingMoviesCtrl' , function ( $scope, $rootScope, $http ) {
 
-		$rootScope.section = "NowPlayingMovies";
+		$rootScope.section = "Now Playing Movies";
+
+		$scope.nowPlayingMovies = "";
+
+		var urlNowPlayingMovies = 'http://api.themoviedb.org/3/discover/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&sort_by=upcoming.desc';
+		$http.get( urlNowPlayingMovies )
+			.then( function( dataNowPlayingMovies ) {
+				$scope.nowPlayingMovies = dataNowPlayingMovies.data.results;
+				console.log( $scope.nowPlayingMovies );
+			})
 	
 	})
 
 	.controller( 'topRattedMoviesCtrl' , function ( $scope, $rootScope, $http ) {
 
-		$rootScope.section = "TopRattedMovies";
+		$rootScope.section = "Top Ratted Movies";
+
+		$scope.topRattedMovies = "";
+
+		var urlTopRattedMovies = 'http://api.themoviedb.org/3/discover/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&sort_by=vote_average.desc';
+		$http.get( urlTopRattedMovies )
+			.then( function( dataTopRattedMovies ) {
+				$scope.topRattedMovies = dataTopRattedMovies.data.results;
+				console.log( $scope.topRattedMovies );
+			})
 	
 	})
 
